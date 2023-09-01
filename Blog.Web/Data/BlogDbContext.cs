04ebca2e7765658dc9bd798da775a9570d78d26f
raloliver <raloliver.ws@gmail.com>
2023-09-01 16:56:18 +0100

    Creating the DBContext Class

---
+++ Blog.Web/Data/BlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // This two props create the tables on database
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
